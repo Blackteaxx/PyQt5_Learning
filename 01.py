22a6f0e7f8c57ab2c76b126f44309766d0a5fc45
Blackteaxx <3110500032@qq.com>
2022-10-29 15:54:09 +0800

    1

--- 01.py
+++ 01.py
@@ -1,6 +1,7 @@
 from PyQt5.Qt import *
 import sys
 
+# 创建一个应用程序
 app = QApplication(sys.argv)
 
 # 窗口控件
@@ -13,6 +14,7 @@ window.move(400, 200)
 label = QLabel(window)
 label.setText("hello world")
 label.move(200, 200)
-
 window.show()
+
+# 执行并进入消息循环
 sys.exit(app.exec_())
