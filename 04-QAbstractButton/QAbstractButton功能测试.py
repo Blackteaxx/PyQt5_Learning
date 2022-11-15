2dd7a2a655b58c6f03a9640f4d45ef11049ea3f1
Blackteaxx <211820073@smail.nju.edu.cn>
2022-11-16 00:34:37 +0800

    1

--- 04-QAbstractButton/QAbstractButton功能测试.py
+++ 04-QAbstractButton/QAbstractButton功能测试.py
@@ -42,6 +42,45 @@ btn.pressed.connect(lambda: print("pressed"))
 # btn.setText('&abc')
 btn.setShortcut('Ctrl+A')
 # ************快捷键设定*******************end
+
+
+# ************自动重复*******************start
+btn.setAutoRepeat(True)
+btn.setAutoRepeatDelay(2000)  # 毫秒为单位
+btn.setAutoRepeatInterval(1000)  # 监测间隔
+print(btn.autoRepeat())
+print(btn.autoRepeatDelay())
+print(btn.autoRepeatInterval())
+# ************自动重复*******************end
+
+# ************排他性设置*******************start
+for i in range(3):
+    btn = QPushButton(window)
+    btn.setText(("btn" + str(i)))
+    btn.move(50 * i, 50 * i)
+
+    print(btn.autoExclusive())
+    btn.setCheckable(True)
+    btn.setAutoExclusive(True)
+
+btn = QPushButton(window)
+btn.setText("btn3")
+btn.setCheckable(True)
+btn.move(150, 150)
+# ************排他性设置*******************end
+
+
+# ************按钮模拟点击*******************start
+btn.click()
+btn.animateClick(2000)  # 延迟松开
+# ************按钮模拟点击*******************end
+
+# ************可用信号*******************start
+# btn.pressed()
+# btn.released()
+btn.clicked.connect(lambda value: print(value))  # 会发射一个值给外界,值为是否被选中
+btn.toggled.connect(lambda value: print("按钮选中状态发生了改变", value))
+# ************可用信号*******************end
 # 2.3 展示控件
 window.show()
 
